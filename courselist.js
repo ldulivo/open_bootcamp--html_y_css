f68c783a947a13a2bbd0a514f29c1fac1775eb93
ldulivo <ldulivo@gmail.com>
2022-06-28 16:26:27 -0300

    04 - Introducción CSS

--- courselist.js
+++ courselist.js
@@ -1,18 +1,22 @@
 const list = [
   {
-    link: './01_Introduccion/index.html',
+    link: './01_Introduccion',
     title: 'Introducción a HTML'
   },
   {
-    link: './02_formularios_tablas/01/index.html',
+    link: './02_formularios_tablas/01',
     title: 'Formularios y Tablas: 01'
   },
   {
-    link: './02_formularios_tablas/02/index.html',
+    link: './02_formularios_tablas/02',
     title: 'Formularios y Tablas: 02'
   },
   {
     link: './03_multimedia',
     title: 'Multimedia'
+  },
+  {
+    link: './04_intro_css',
+    title: 'Introducción CSS'
   }
 ]
